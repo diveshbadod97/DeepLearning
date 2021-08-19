bc158364c39809d5c99d9312b2703ead520c14be
db9932 <db9932@rit.edu>
2021-08-19 17:11:40 -0400

    GANS added

---
+++ Practice7/pytorchbasics.py
@@ -0,0 +1,131 @@
+import torch
+import torchvision
+from torch import nn, optim
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor, Lambda, Compose, transforms
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn.functional as F
+
+# Download the FashionMNIST dataset from torchvision.datasets
+training_data = datasets.FashionMNIST('./data', train=True, download=True, transform=
+                                                transforms.Compose([transforms.ToTensor()]))
+
+# Download the FashionMNIST dataset from torchvision.datasets
+test_data = datasets.FashionMNIST('./data', train=False, download=True, transform=
+                                                transforms.Compose([transforms.ToTensor()]))
+
+# Create data loaders.
+batch_size = 32
+train_dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=True)
+test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
+
+# If the dataloader is working, the size of tensor X should be: torch.Size([batch_size, 1, 28, 28])
+for X, y in test_dataloader:
+    print("Shape of X [N, C, H, W]: ", X.shape)
+    print("Shape of y: ", y.shape, y.dtype)
+    break
+
+# Get cpu or gpu device for training.
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print("Using {} device".format(device))
+
+
+# Define model
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+
+        self.fc1 = nn.Linear(in_features=64 * 6 * 6, out_features=600)
+        self.drop = nn.Dropout2d(0.25)
+        self.fc2 = nn.Linear(in_features=600, out_features=120)
+        self.fc3 = nn.Linear(in_features=120, out_features=10)
+
+    def forward(self, x):
+        # Define the forward pass such that it returns scores for each class
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.drop(out)
+        out = self.fc2(out)
+        out = self.fc3(out)
+        return out
+
+
+model = NeuralNetwork().to(device)
+print(model)
+
+
+def train(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+
+def test(dataloader, model):
+    size = len(dataloader.dataset)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= size
+    correct /= size
+    print(f"Test Results: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+epochs = 10
+for t in range(epochs):
+    print(f"Epoch {t + 1}\n-------------------------------")
+    train(train_dataloader, model, loss_fn, optimizer)
+    test(test_dataloader, model)
+print("Done!")
+
+# Predictions for first 20 images in the test set
+classes = [
+    "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot",
+]
+
+model.eval()
---
+++ Practice7/vanilla_gan.py
@@ -0,0 +1,403 @@
+# Usage:
+# ======
+#    To train with the default hyperparamters (saves results to checkpoints_vanilla/ and samples_vanilla/):
+#       python vanilla_gan.py
+
+import os
+import pdb
+import pickle
+import argparse
+
+import warnings
+
+from torch.utils.data import dataloader
+
+warnings.filterwarnings("ignore")
+
+# Numpy
+import numpy as np
+from PIL import Image
+
+# Torch imports
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from numpy import (amin, amax, ravel, asarray, arange, ones, newaxis,
+                   transpose, iscomplexobj, uint8, issubdtype, array)
+from PIL import Image
+
+# Local imports
+import utils
+from data_loader import get_emoji_loader
+from models import DCGenerator, DCDiscriminator
+import matplotlib.pyplot as plt
+
+SEED = 11
+
+# Set the random seed manually for reproducibility.
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(SEED)
+
+
+###### Dont change this part #######
+
+def bytescale(data, cmin=None, cmax=None, high=255, low=0):
+    if data.dtype == uint8:
+        return data
+
+    if high > 255:
+        raise ValueError("`high` should be less than or equal to 255.")
+    if low < 0:
+        raise ValueError("`low` should be greater than or equal to 0.")
+    if high < low:
+        raise ValueError("`high` should be greater than or equal to `low`.")
+
+    if cmin is None:
+        cmin = data.min()
+    if cmax is None:
+        cmax = data.max()
+
+    cscale = cmax - cmin
+    if cscale < 0:
+        raise ValueError("`cmax` should be larger than `cmin`.")
+    elif cscale == 0:
+        cscale = 1
+
+    scale = float(high - low) / cscale
+    bytedata = (data - cmin) * scale + low
+    return (bytedata.clip(low, high) + 0.5).astype(uint8)
+
+
+def toimage(arr, high=255, low=0, cmin=None, cmax=None, pal=None,
+            mode=None, channel_axis=None):
+    data = asarray(arr)
+    if iscomplexobj(data):
+        raise ValueError("Cannot convert a complex-valued array.")
+    shape = list(data.shape)
+    valid = len(shape) == 2 or ((len(shape) == 3) and
+                                ((3 in shape) or (4 in shape)))
+    if not valid:
+        raise ValueError("'arr' does not have a suitable array shape for "
+                         "any mode.")
+    if len(shape) == 2:
+        shape = (shape[1], shape[0])  # columns show up first
+        if mode == 'F':
+            data32 = data.astype(np.float32)
+            image = Image.frombytes(mode, shape, data32.tostring())
+            return image
+        if mode in [None, 'L', 'P']:
+            bytedata = bytescale(data, high=high, low=low,
+                                 cmin=cmin, cmax=cmax)
+            image = Image.frombytes('L', shape, bytedata.tostring())
+            if pal is not None:
+                image.putpalette(asarray(pal, dtype=uint8).tostring())
+                # Becomes a mode='P' automagically.
+            elif mode == 'P':  # default gray-scale
+                pal = (arange(0, 256, 1, dtype=uint8)[:, newaxis] *
+                       ones((3,), dtype=uint8)[newaxis, :])
+                image.putpalette(asarray(pal, dtype=uint8).tostring())
+            return image
+        if mode == '1':  # high input gives threshold for 1
+            bytedata = (data > high)
+            image = Image.frombytes('1', shape, bytedata.tostring())
+            return image
+        if cmin is None:
+            cmin = amin(ravel(data))
+        if cmax is None:
+            cmax = amax(ravel(data))
+        data = (data * 1.0 - cmin) * (high - low) / (cmax - cmin) + low
+        if mode == 'I':
+            data32 = data.astype(np.uint32)
+            image = Image.frombytes(mode, shape, data32.tostring())
+        else:
+            raise ValueError("_errstr")
+        return image
+
+    # if here then 3-d array with a 3 or a 4 in the shape length.
+    # Check for 3 in datacube shape --- 'RGB' or 'YCbCr'
+    if channel_axis is None:
+        if (3 in shape):
+            ca = np.flatnonzero(asarray(shape) == 3)[0]
+        else:
+            ca = np.flatnonzero(asarray(shape) == 4)
+            if len(ca):
+                ca = ca[0]
+            else:
+                raise ValueError("Could not find channel dimension.")
+    else:
+        ca = channel_axis
+
+    numch = shape[ca]
+    if numch not in [3, 4]:
+        raise ValueError("Channel axis dimension is not valid.")
+
+    bytedata = bytescale(data, high=high, low=low, cmin=cmin, cmax=cmax)
+    if ca == 2:
+        strdata = bytedata.tostring()
+        shape = (shape[1], shape[0])
+    elif ca == 1:
+        strdata = transpose(bytedata, (0, 2, 1)).tostring()
+        shape = (shape[2], shape[0])
+    elif ca == 0:
+        strdata = transpose(bytedata, (1, 2, 0)).tostring()
+        shape = (shape[2], shape[1])
+    if mode is None:
+        if numch == 3:
+            mode = 'RGB'
+        else:
+            mode = 'RGBA'
+
+    if mode not in ['RGB', 'RGBA', 'YCbCr', 'CMYK']:
+        raise ValueError("_errstr")
+
+    if mode in ['RGB', 'YCbCr']:
+        if numch != 3:
+            raise ValueError("Invalid array shape for mode.")
+    if mode in ['RGBA', 'CMYK']:
+        if numch != 4:
+            raise ValueError("Invalid array shape for mode.")
+
+    # Here we know data and mode is correct
+    image = Image.frombytes(mode, shape, strdata)
+    return image
+
+
+#################################
+
+
+def print_models(G, D):
+    """Prints model information for the generators and discriminators.
+    """
+    print("                    G                  ")
+    print("---------------------------------------")
+    print(G)
+    print("---------------------------------------")
+
+    print("                    D                  ")
+    print("---------------------------------------")
+    print(D)
+    print("---------------------------------------")
+
+
+def create_model(opts):
+    """Builds the generators and discriminators.
+    """
+    G = DCGenerator(noise_size=opts.noise_size, conv_dim=opts.conv_dim)
+    D = DCDiscriminator(conv_dim=opts.conv_dim)
+
+    print_models(G, D)
+
+    if torch.cuda.is_available():
+        G.cuda()
+        D.cuda()
+        print('Models moved to GPU.')
+
+    return G, D
+
+
+def checkpoint(iteration, G, D, opts):
+    """Saves the parameters of the generator G and discriminator D.
+    """
+    G_path = os.path.join(opts.checkpoint_dir, 'G.pkl')
+    D_path = os.path.join(opts.checkpoint_dir, 'D.pkl')
+    torch.save(G.state_dict(), G_path)
+    torch.save(D.state_dict(), D_path)
+
+
+def create_image_grid(array, ncols=None):
+    """
+    """
+    num_images, channels, cell_h, cell_w = array.shape
+
+    if not ncols:
+        ncols = int(np.sqrt(num_images))
+    nrows = int(np.math.floor(num_images / float(ncols)))
+    result = np.zeros((cell_h * nrows, cell_w * ncols, channels), dtype=array.dtype)
+    for i in range(0, nrows):
+        for j in range(0, ncols):
+            result[i * cell_h:(i + 1) * cell_h, j * cell_w:(j + 1) * cell_w, :] = array[i * ncols + j].transpose(1, 2,
+                                                                                                                 0)
+
+    if channels == 1:
+        result = result.squeeze()
+    return result
+
+
+def scale(x, feature_range=(-1, 1)):
+    ''' Scale takes in an image x and returns that image, scaled
+       with a feature_range of pixel values from -1 to 1. 
+       This function assumes that the input x is already scaled from 0-1.'''
+    # assume x is scaled to (0, 1)
+    # scale to feature_range and return scaled x
+    min, max = feature_range
+    x = x * (max - min) + min
+    return x
+
+
+def save_samples(G, fixed_noise, iteration, opts):
+    generated_images = G(fixed_noise)
+    generated_images = utils.to_data(generated_images)
+
+    grid = create_image_grid(generated_images)
+
+    # merged = merge_images(X, fake_Y, opts)
+    path = os.path.join(opts.sample_dir, 'sample-{:06d}.png'.format(iteration))
+    # scipy.misc.imsave(path, grid)
+    print('Saved {}'.format(path))
+
+
+def sample_noise(dim):
+    """
+    Generate a PyTorch Variable of uniform random noise.
+
+    Input:
+    - batch_size: Integer giving the batch size of noise to generate.
+    - dim: Integer giving the dimension of noise to generate.
+
+    Output:
+    - A PyTorch Variable of shape (batch_size, dim, 1, 1) containing uniform
+      random noise in the range (-1, 1).
+    """
+    return utils.to_var(torch.rand(batch_size, dim) * 2 - 1).unsqueeze(2).unsqueeze(3)
+
+
+def training_loop(train_dataloader, opts):
+    """Runs the training loop.
+        * Saves checkpoints every opts.checkpoint_every iterations
+        * Saves generated samples every opts.sample_every iterations
+    """
+
+    # Create generators and discriminators
+    G, D = create_model(opts)
+
+    # Create optimizers for the generators and discriminators
+    g_optimizer = optim.Adam(G.parameters(), opts.lr, [opts.beta1, opts.beta2])
+    d_optimizer = optim.Adam(D.parameters(), opts.lr, [opts.beta1, opts.beta2])
+
+    # Generate fixed noise for sampling from the generator
+    fixed_noise = sample_noise(opts.noise_size)  # batch_size x noise_size x 1 x 1
+
+    iteration = 1
+    total_train_iters = opts.num_epochs * len(train_dataloader)
+    for epoch in range(opts.num_epochs):
+        print('Epoch number: {}'.format(epoch))
+        for batch in train_dataloader:
+
+            #############################################
+            #######Get the data and convert to variables#
+            #############################################
+            ################ TODO #######################
+            real_images, real_labels = batch
+            real_images, real_labels = utils.to_var(real_images), utils.to_var(real_labels).long().squeeze()
+
+            ################################################
+            ###         TRAIN THE DISCRIMINATOR         ####
+            ################################################
+
+            # Discriminator loss on the real images
+            D_real_loss = 0.5 * torch.mean((D(real_images) - 1) ** 2)  # mean calculate 1/m * sum
+
+            # 2. Sample noise
+            noise = sample_noise(100)
+
+            # 3. Generate fake images from the noise
+            fake_images = G(noise)
+
+            # 4. Compute the discriminator loss on the fake images
+            D_fake_loss = 0.5 * torch.mean((D(fake_images)) ** 2)
+
+            # 5. Compute the total discriminator loss
+            D_total_loss = D_real_loss + D_fake_loss
+
+            D_total_loss.backward()
+            d_optimizer.step()
+
+            ###########################################
+            ###          TRAIN THE GENERATOR        ###
+            ###########################################
+
+            g_optimizer.zero_grad()
+
+            # FILL THIS IN
+            # Sample noise
+            ## TODO######
+            noise = sample_noise(100)
+
+            # Compute the generator loss
+            G_loss = torch.mean((D(G(noise)) - 1) ** 2)
+            G_loss.backward()
+            g_optimizer.step()
+
+            # Print the log info
+            if iteration % opts.log_step == 0:
+                print('Iteration [{:4d}/{:4d}] | D_real_loss: {:6.4f} | D_fake_loss: {:6.4f} | G_loss: {:6.4f}'.format(
+                    iteration, total_train_iters, D_real_loss.item(), D_fake_loss.item(), G_loss.item()))
+
+            # Save the generated samples
+            if iteration % opts.sample_every == 0:
+                save_samples(G, noise, iteration, opts)
+
+            # Save the model parameters
+            if iteration % opts.checkpoint_every == 0:
+                checkpoint(iteration, G, D, opts)
+
+            iteration += 1
+    save_samples(G, noise, iteration, opts)
+
+
+def main(opts):
+    """Loads the data, creates checkpoint and sample directories, and starts the training loop.
+    """
+
+    # Create a dataloader for the training images
+    train_dataloader, _ = get_emoji_loader(opts.emoji, opts)
+
+    # Create checkpoint and sample directories
+    utils.create_dir(opts.checkpoint_dir)
+    utils.create_dir(opts.sample_dir)
+
+    training_loop(train_dataloader, opts)
+
+
+def create_parser():
+    """Creates a parser for command-line arguments.
+    """
+    parser = argparse.ArgumentParser()
+
+    # Model hyper-parameters
+    parser.add_argument('--image_size', type=int, default=32, help='The side length N to convert images to NxN.')
+    parser.add_argument('--conv_dim', type=int, default=32)
+    parser.add_argument('--noise_size', type=int, default=100)
+
+    # Training hyper-parameters
+    parser.add_argument('--num_epochs', type=int, default=40)
+    parser.add_argument('--batch_size', type=int, default=16, help='The number of images in a batch.')
+    parser.add_argument('--num_workers', type=int, default=0, help='The number of threads to use for the DataLoader.')
+    parser.add_argument('--lr', type=float, default=0.0003, help='The learning rate (default 0.0003)')
+    parser.add_argument('--beta1', type=float, default=0.5)
+    parser.add_argument('--beta2', type=float, default=0.999)
+
+    # Data sources
+    parser.add_argument('--emoji', type=str, default='Apple', choices=['Apple', 'Facebook', 'Windows'],
+                        help='Choose the type of emojis to generate.')
+
+    # Directories and checkpoint/sample iterations
+    parser.add_argument('--checkpoint_dir', type=str, default='./checkpoints_vanilla')
+    parser.add_argument('--sample_dir', type=str, default='./samples_vanilla')
+    parser.add_argument('--log_step', type=int, default=10)
+    parser.add_argument('--sample_every', type=int, default=200)
+    parser.add_argument('--checkpoint_every', type=int, default=400)
+
+    return parser
+
+
+if __name__ == '__main__':
+    parser = create_parser()
+    opts = parser.parse_args()
+
+    batch_size = opts.batch_size
+
+    print(opts)
+    main(opts)
